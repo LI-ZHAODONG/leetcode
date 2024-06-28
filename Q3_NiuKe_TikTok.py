9bf8c0948b910d3225d82e6be923500ded59306d
LI ZHAODONG <LI-ZHAODONG>
2024-06-28 17:27:00 +0800

    a bit Q3

---
+++ Q3_NiuKe_TikTok.py
@@ -0,0 +1,39 @@
+import sys
+
+input_line = sys.stdin.read().split()
+input_lines = []
+for x in input_line:
+    input_lines.append(int(x))
+print(input_lines)
+
+for add in range(1, 10, 1):
+    input_lines.append(add)
+    for QT in range(1, 10, 1):
+
+# while True:
+#     if True:
+#         input_lines.append(1)
+#
+#     if True:
+#         input_lines.append(2)
+#
+#     if True:
+#         input_lines.append(3)
+#
+#     if True:
+#         input_lines.append(4)
+#
+#     if True:
+#         input_lines.append(5)
+#
+#     if True:
+#         input_lines.append(6)
+#
+#     if True:
+#         input_lines.append(7)
+#
+#     if True:
+#         input_lines.append(8)
+#
+#     if True:
+#         input_lines.append(9)
